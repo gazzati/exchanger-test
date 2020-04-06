0b0765a1a3606562f03b37a78d816bd39c09de1c
Timur <gazzaevtimur@gmail.com>
2020-04-06 19:45:22 +0300

    second commit with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Nav} from "react-bootstrap";
+import MainTable from "./MainTable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showButton, setShowButton] = useState(false);
+    const [showTab, setShowTab] = useState(false);
+    const [preValue, setPreValue] = useState(null)
+    const [value, setValue] = useState(null)
+
+
+    const onSelectNav = (selectedKey) => {
+        setPreValue(selectedKey);
+        setShowButton(true);
+    }
+
+    const onGetRatesClick = () => {
+        setShowTab(true);
+        setValue(preValue)
+    }
+
+    return (
+        <div className="App">
+            <div className="SelectValue">
+                <Nav className="Selector" variant="pills"
+                     onSelect={onSelectNav}>
+                    <Nav.Item>
+                        <Nav.Link eventKey="USD">USD</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="EUR">EUR</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="RUB">RUB</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                {showButton &&
+                <Button className="GetButton" onClick={onGetRatesClick}>Get rates</Button>}
+            </div>
+            <div className="WrapperContent">
+                {showTab && <MainTable currency={value} />}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/MainTable.jsx
+++ src/MainTable.jsx
@@ -1,45 +1,97 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Nav, Tab, Tabs} from "react-bootstrap";
-import TopFiveValues from "./TopFiveValues";
+import {Button, Tab, Table, Tabs} from "react-bootstrap";
+import UnitOfTable from "./UnitOfTable";
 
-function App() {
-    const [showTab, setShowTab] = useState(false);
-    const [value, setValue] = useState(null)
 
-    const onSelectNav = (selectedKey) => {
-        alert(`selected ${selectedKey}`);
-        setValue(selectedKey);
-        setShowTab(true);
+function MainTable({currency}) {
+    const [rates, setRates] = useState({rates: {}, date: null})
+    const [preRates, setPreRates] = useState({rates: {}})
+    const [prePreRates, setPrePreRates] = useState({rates: {}})
+    const [pastMode, setPastMode] = useState(false)
+
+    const pastModeChanger = () => {
+        setPastMode(!pastMode)
     }
+
+    const changeFormatTime = (value) => {
+        if (value < 10) {
+            return `0${value}`
+        } else return value
+    }
+
+    const changeDate = (date) => {
+        const newDate = new Date(date)
+        newDate.setDate(newDate.getDate() - 1)
+        return `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${newDate.getDate()}`
+    }
+
+    const changeDateToTable = (date) => {
+        const newDate = new Date(date)
+        return `${changeFormatTime(newDate.getDate())}-${changeFormatTime(newDate.getMonth() + 1)}-${newDate.getFullYear()}`
+    }
+
+    useEffect(() => {
+        const getRates = async (setRates, date = 'latest') => {
+            try {
+                let response = await fetch(`https://api.exchangeratesapi.io/${date}?base=${currency}`)
+                let answer = await response.json();
+                setRates({
+                    ...answer,
+                    date: changeDateToTable(answer.date)
+                });
+                console.log(answer.date)
+                return answer.date
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        getRates(setRates)
+            .then(response => getRates(setPreRates, changeDate(response))
+                .then(response => getRates(setPrePreRates, changeDate(response))))
+
+    }, [currency])
     return (
-    <div className="App">
-        <Nav
-            onSelect={onSelectNav}>
-            <Nav.Item>
-                <Nav.Link eventKey="USD">USD</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="EUR">EUR</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="RUB">RUB</Nav.Link>
-            </Nav.Item>
-        </Nav>
-
-        {showTab && <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-            <Tab eventKey="home" title="Home">
-                {value} <>
+        <Tabs defaultActiveKey="rates" id="uncontrolled-tab-example">
+            <Tab eventKey="rates" title="Exchange rates">
+                <div className="WrapperContent">
+                    <div className="Description">
+                        <span>Base currency: <b>{currency}</b></span>
+                        <span className="margin-left">Request date: <b>{`${changeDateToTable(new Date())}`}</b></span>
+                        <span className="margin-left">Request time: <b>{`${changeFormatTime(
+                            (new Date().getHours()))}:${changeFormatTime(new Date().getMinutes())}`}</b></span>
+                        {!pastMode
+                            ? <Button variant="primary"
+                                      className="margin-left"
+                                      onClick={pastModeChanger}>{preRates.date} rates</Button>
+                            : <Button className="margin-left"
+                                      variant="primary"
+                                      onClick={pastModeChanger}>{rates.date} rates</Button>}
+                    </div>
+                    <Table className="MainTable" striped bordered>
+                        <thead>
+                        <tr>
+                            <th>Currency</th>
+                            <th>Rates on {!pastMode ? rates.date : preRates.date}</th>
+                            <th>In comparison with {!pastMode ? preRates.date : prePreRates.date}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {rates && Object.keys(rates.rates)
+                            .map(el => (el != currency) && <UnitOfTable values={el}
+                                                                        rates={pastMode ? preRates.rates[el] : rates.rates[el]}
+                                                                        preRates={pastMode ? prePreRates.rates[el] : preRates.rates[el]}/>)
+                        }
+                        </tbody>
+                    </Table>
+                </div>
             </Tab>
-            <Tab eventKey="profile" title="Profile">
-                222 <TopFiveValues/>
-            </Tab>
-
-        </Tabs>}
-
-    </div>
-  );
+            {/* <Tab eventKey="topFive" title="Top 5 biggest changes">
+        </Tab>*/}
+        </Tabs>
+    );
 }
 
-export default App;
+export default MainTable;
