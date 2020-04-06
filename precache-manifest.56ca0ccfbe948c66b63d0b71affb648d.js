18aa243c77554c38d83b0ade180c614ad252b0b2
Timur <gazzaevtimur@gmail.com>
2020-04-06 19:46:06 +0300

    Updates

---
+++ precache-manifest.56ca0ccfbe948c66b63d0b71affb648d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bb3c0345e947412f353d1428180f0f",
+    "url": "/exchanger-test/index.html"
+  },
+  {
+    "revision": "6c6241686ef8538cda6b",
+    "url": "/exchanger-test/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7949139f39965ed8a54b",
+    "url": "/exchanger-test/static/css/main.813d18c3.chunk.css"
+  },
+  {
+    "revision": "6c6241686ef8538cda6b",
+    "url": "/exchanger-test/static/js/2.07cd43ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/exchanger-test/static/js/2.07cd43ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7949139f39965ed8a54b",
+    "url": "/exchanger-test/static/js/main.43ddaf99.chunk.js"
+  },
+  {
+    "revision": "67c0f9cc31f6e39088f3",
+    "url": "/exchanger-test/static/js/runtime-main.76dd20e8.js"
+  }
+]);
